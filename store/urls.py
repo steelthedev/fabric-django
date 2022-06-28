1576a056cae1cb507a0d12a1703397a06da1357f
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2022-06-28 10:15:28 +0100

    added product creationg

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "store"
 urlpatterns = [
     path('', views.ProductView, name="product"),
+    path('create-product', views.AddProducts, name="add_product"),
     path('login', views.LoginView, name="login" )
 ]
 
--- store/views.py
+++ store/views.py
@@ -1,6 +1,5 @@
-
-from multiprocessing import context
-from wsgiref import headers
+import datetime
+from urllib import response
 from django.shortcuts import redirect, render
 import requests
 import json
@@ -64,3 +63,67 @@ def ProductView(request):
         products = response_data
         
     return render(request,'store/products.html',products)
+
+
+def AddProducts(request):
+    if request.method == "POST":
+        sku = request.POST["sku"]
+        title = request.POST["title"]
+        nodeName = request.POST["nodeName"]
+        image = request.POST["image"]
+        price = request.POST["price"]
+        weight = request.POST["weight"]
+        status = request.POST["status"]
+
+        print(nodeName)
+
+
+        base_url = settings.BASE_URL
+        path = f"api-product/v1/product/bulk/insert"
+        url = base_url + path
+        token = request.session["auth_token"]
+        date =datetime.datetime.now()
+        d=date.strftime("%m/%d/%Y, %H:%M:%S")
+        headers ={
+          
+            "x-site-context":json.dumps({"stage":"sandbox", "channel":12, "account":"60ec78e9fb4cf000085cf0b9",  "date":d}),
+            "Content-Type":"application/json"
+        }
+
+        body =[
+                 {
+                "sku": sku,
+                "type": "ITEM",
+                "nodeName":nodeName,
+                "attributeValues": [
+                {
+                    "name": "title",
+                    "value": title
+                },
+                  {
+                    "name": "image",
+                    "value": image
+                },
+                  {
+                    "name": "price",
+                    "value": price
+                },
+                  {
+                    "name": "weight",
+                    "value": weight
+                },
+                  {
+                    "name": "status",
+                    "value": "TRUE"
+                }
+                ],
+            }
+            ]
+            
+
+        response = requests.post(url, headers=headers, json = body)
+
+        if response.status_code == 200:
+            r = response.json()
+            print(r)
+    return render(request,'store/create_products.html')
